3bf3ccbb4a2c95670ea83adbfa7c5bf4fb209532
Shajeenth <89612702+Shajeenth@users.noreply.github.com>
2026-07-31 18:41:25 -0400

    libertinus font change

--- src/components/ProjectScroll.tsx
+++ src/components/ProjectScroll.tsx
@@ -6,7 +6,7 @@ function ProjectScroll() {
         <section className="py-8">
             <div className="container mx-auto px-4">
                 <div className="mt-0">
-                    <h1 className="text-6xl heading mb-8 mt-0 text-[#fdf8c2]">projects</h1>
+                    <h1 className="text-6xl heading mb-8 mt-0 text-[#fdf8c2]" style={{ fontFamily: 'Libertinus Math, serif' }}>projects</h1>
                 </div>
                 <div className="flex justify-center">
                     <Project image="src/assets/img2048.png" alt="imageOf2048" header="2048" text="A simple web version of 2048 made using javascript."/>
