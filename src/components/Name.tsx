b3cc1582443b840c53863c91a2a2f5a238e8f0eb
Shajeenth <89612702+Shajeenth@users.noreply.github.com>
2026-07-31 23:03:28 -0400

    i forgot

--- src/components/Name.tsx
+++ src/components/Name.tsx
@@ -14,7 +14,7 @@ function Name() {
                 <div className="mb-5 h-[1px] w-full max-w-72 rounded-full bg-gradient-to-r from-[#fdf8c2] via-[#d4a017] to-transparent opacity-90" />
 
                 <p className="tinytext text-white">
-                    <span className="text-gray-300">mechatronics @ </span>
+                    <span className="text-gray-300">mechatronics engineering @ </span>
                     <span className="font-semibold text-white">UWaterloo</span>
                 </p>
             </div>
